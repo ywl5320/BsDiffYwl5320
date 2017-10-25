93155d53b2f41b3a240f7b6376c04f71fb55d74b
596353036@qq.com <ywl12345>
2017-10-25 17:19:40 +0800

    test add library

---
+++ bspatchlibrary/src/main/java/com/ywl5320/bspatch/TsetUtil.java
@@ -0,0 +1,14 @@
+package com.ywl5320.bspatch;
+
+import android.util.Log;
+
+/**
+ * Created by ywl5320 on 2017/10/25.
+ */
+
+public class TsetUtil {
+
+    public void printfinfo() {
+        Log.d("ywl5320", "library");
+    }
+}
